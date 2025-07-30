a759b4fc8dd276f8322dce9aea08de87fc5ea58c
Team-2383 <106979512+Team-2383@users.noreply.github.com>
2025-07-30 09:53:09 -0400

    update autos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/AutoCommands.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/AutoCommands.java
@@ -5,8 +5,10 @@ import com.pedropathing.localization.Pose;
 
 import org.firstinspires.ftc.teamcode.subsystems.Subsystems;
 import org.firstinspires.ftc.teamcode.subsystems.drive.Drive;
+import org.firstinspires.ftc.teamcode.subsystems.intake.Intake;
 import org.firstinspires.ftc.teamcode.subsystems.pivot.Pivot;
 import org.firstinspires.ftc.teamcode.subsystems.pivot.PivotConstants;
+import org.firstinspires.ftc.teamcode.subsystems.shooter.Shooter;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -20,7 +22,7 @@ public class AutoCommands {
     DriveCommands.forward(subsystems.drive(), distance),
     DriveCommands.backward(subsystems.drive(), distance),
     DriveCommands.strafeLeft(subsystems.drive(), distance),
-    DriveCommands.strafeRight(subsystems.drive(), distance),
+    DriveCommands.strafeRight(subsystems.drive(), distance)
 
     (Replace angle with a number):
     DriveCommands.turn(subsystems.drive(), angle),
@@ -51,13 +53,53 @@ public class AutoCommands {
     */
     public static Command blueAuto(Subsystems subsystems) {
         return Commands.sequence(
-                DriveCommands.forward(subsystems.drive(), 10)
+                Shooter.setPower(subsystems.shooter(), () -> -0.73).withTimeout(0.5),
+                DriveCommands.strafeLeft(subsystems.drive(), 4),
+                DriveCommands.backward(subsystems.drive(), 4),
+                DriveCommands.turn(subsystems.drive(), -35),
+                DriveCommands.backward(subsystems.drive(), 65),
+
+                Intake.setPower(subsystems.intake(), () -> 0.4).withTimeout(3.0),
+                Shooter.setPower(subsystems.shooter(), () -> 0).withTimeout(0.1),
+                DriveCommands.forward(subsystems.drive(), 11),
+                DriveCommands.turn(subsystems.drive(),-150),
+                DriveCommands.forward(subsystems.drive(), 30),
+                Intake.setPower(subsystems.intake(), () -> 0.35).withTimeout(1.5),
+                Shooter.setPower(subsystems.shooter(), () -> 0.3).withTimeout(0.8),
+                Intake.setPower(subsystems.intake(), () -> -0.25).withTimeout(0.6),
+                Shooter.setPower(subsystems.shooter(), () -> 0).withTimeout(0.1),
+                Intake.setPower(subsystems.intake(), () -> 0).withTimeout(0.1),
+                DriveCommands.backward(subsystems.drive(), 30),
+                DriveCommands.turn(subsystems.drive(), 150),
+                Shooter.setPower(subsystems.shooter(), () -> 0.73).withTimeout(1.5),
+                Intake.setPower(subsystems.intake(), () -> 0.6).withTimeout(0.8)
+
+
         );
     }
-
+public static double red_ball= 90;
     public static Command redAuto(Subsystems subsystems) {
         return Commands.sequence(
-                DriveCommands.forward(subsystems.drive(), 10)
+                Shooter.setPower(subsystems.shooter(), () -> -0.7).withTimeout(0.5),
+                DriveCommands.strafeLeft(subsystems.drive(), -4),
+                DriveCommands.backward(subsystems.drive(), 4),
+                DriveCommands.turn(subsystems.drive(), 37),
+                DriveCommands.backward(subsystems.drive(), 65),
+                Intake.setPower(subsystems.intake(), () -> 0.4).withTimeout(3.0),
+                Shooter.setPower(subsystems.shooter(), () -> 0).withTimeout(0.1),
+                DriveCommands.forward(subsystems.drive(), 11),
+                DriveCommands.turn(subsystems.drive(),  red_ball),
+                Intake.setPower(subsystems.intake(), () -> 0.35).withTimeout(0),
+                DriveCommands.forward(subsystems.drive(), 27),
+                Shooter.setPower(subsystems.shooter(), () -> 0.3).withTimeout(0.0),
+                Intake.setPower(subsystems.intake(), () -> -0.25).withTimeout(0.3),
+                Shooter.setPower(subsystems.shooter(), () -> 0).withTimeout(0),
+                Intake.setPower(subsystems.intake(), () -> 0).withTimeout(0.1),
+                DriveCommands.backward(subsystems.drive(), 27),
+                DriveCommands.turn(subsystems.drive(), 90),
+                Shooter.setPower(subsystems.shooter(), () -> 0.73).withTimeout(1.5),
+                Intake.setPower(subsystems.intake(), () -> 0.6).withTimeout(0.8)
+
         );
     }
 }
